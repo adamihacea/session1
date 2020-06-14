073ace773002cd6aa87cb1bb89c4c801f15952d4
Ada Mihacea <ada.mihacea@ebs.msft>
2020-06-14 23:47:19 +0300

    TEMA PY

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#2. Get the number of occurrences of var b in array a.
+
+a = [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,]
+b = 4
+print(a.count(b))
+c = 2
+print(a.count(c))
+d = 5
+print(a.count(d))
---
+++ ex3.py
@@ -0,0 +1,10 @@
+#3. Given a sentence as string, count the number of words in it.
+
+
+a = 'There are few things better in life than a slice of pie.'
+print(len(a.split()))
+
+#print(len(a)) numara toate elem cu tot cu spatii
+#print(a.split()) identifica fiecare cuvant
+
+
